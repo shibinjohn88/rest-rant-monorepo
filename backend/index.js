2efc050c1c8f488c53144ddfcb73b5a2142615d0
shibinjohn88 <shibinjohnpdlm@gmail.com>
2023-03-08 21:19:15 -0500

    authentication implemented using jwt tokens

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const app = express();
+const app = express()
+
 
 // Express Settings
 app.use(cors())
--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -30,6 +30,8 @@ function LoginForm() {
 
             if (response.status === 200) {
                 setCurrentUser(data.user)
+                console.log(data.token)
+                localStorage.setItem('token', data.token)
                 history.push('/')
             }
             else {
